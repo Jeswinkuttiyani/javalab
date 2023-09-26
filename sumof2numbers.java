5518326423d457133ae5c4aa6153eedb8c8994b5
Jeswinkuttiyani <118618288+Jeswinkuttiyani@users.noreply.github.com>
2023-09-26 14:40:13 +0530

    Add files via upload

---
+++ sumof2numbers.java
@@ -0,0 +1,14 @@
+package java2;
+import java.util.Scanner;
+public class java1 {
+	public static void main(String[] args) {
+		int number1,number2,sum;
+		Scanner Sc=new Scanner(System.in);
+		System.out.println("enter two number");
+		number1=Sc.nextInt();
+		number2=Sc.nextInt();
+		sum=number1+number2;
+		System.out.println("sum is:"+sum);
+		
+	}
+}
