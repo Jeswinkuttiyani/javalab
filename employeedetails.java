03caf5913250fe9d0279acf28e377d0815752691
Jeswinkuttiyani <118618288+Jeswinkuttiyani@users.noreply.github.com>
2023-10-17 16:17:40 +0530

    details of empolyee

--- employeedetails.java
+++ employeedetails.java
@@ -34,13 +34,14 @@ long phoneNo;
 Scanner sc=new Scanner(System.in);
 public void employedetails(){
 	System.out.println("enter the employe name:");
-	name=sc.next();
+	name=sc.nextLine();
 	System.out.println("enter the employe age:");
 	age=sc.nextInt();
 	System.out.println("enter the employe phone number:");
 	phoneNo=sc.nextLong();
 	System.out.println("enter the employe address:");
-	address=sc.next();
+	address=sc.nextLine();
+	sc.nextLine();
 	System.out.println("enter the employe salary:");
 	salary=sc.nextInt();
 	}
@@ -67,6 +68,7 @@ class Manager extends employee{
 	String department;
 	public void managerdetail() {
 	System.out.println("enter the department:");
+	sc.nextLine();
 	department=sc.next();
 	}
 	public void printdetail() {
