0a89ecc15ae0f5c4d60230be01b716b4729c5805
Jeswinkuttiyani <118618288+Jeswinkuttiyani@users.noreply.github.com>
2023-12-02 10:08:38 +0530

    Create multithreading2.java

---
+++ multithreading2.java
@@ -0,0 +1,80 @@
+package Java2;
+import java.util.Random;
+public class MultiThreadOddEven {
+public static void main(String[]args) {
+	NumberManger numberManger=new NumberManger();
+	NumberGenerator numberGenerator=new NumberGenerator(numberManger);
+	EvenPrinterThread evenPrinterThread=new EvenPrinterThread(numberManger);
+	numberGenerator.start();
+	evenPrinterThread.start();
+	OddPrinterThread oddPrinterThread=new OddPrinterThread(numberManger);
+	oddPrinterThread.start();
+}
+}
+class NumberManger{
+    private int generatedNumber;
+    private boolean numberGenerated=false;
+    public synchronized void generateNumber() {
+    	generatedNumber=new Random().nextInt(99)+2;
+    	System.out.println("Generated random number is: "+generatedNumber);
+    	numberGenerated=true;
+    	notifyAll();
+    }
+    public synchronized void printEvenNumber() throws InterruptedException {
+    	while(!numberGenerated || generatedNumber%2!=0) {
+    		wait();
+    	}
+    		System.out.println("The square of the generated number is: "+generatedNumber*generatedNumber);
+    	numberGenerated=false;
+	}
+    public synchronized void printOddNumber() throws InterruptedException {
+    	while(!numberGenerated || generatedNumber%2==0)
+    		wait();
+    		System.out.println("The cube of the generated number is: "+generatedNumber*generatedNumber*generatedNumber);
+    	numberGenerated=false;
+	}
+    } 
+class NumberGenerator extends Thread{
+	NumberManger numberManger;
+	public NumberGenerator(NumberManger numberManger) {
+		this.numberManger=numberManger;
+	    }
+	public void run() {
+		numberManger.generateNumber();
+		try {
+			Thread.sleep(1000);
+		}
+		catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class EvenPrinterThread extends Thread{
+	NumberManger numberManger;
+	public EvenPrinterThread(NumberManger numberManger) {
+		this.numberManger=numberManger;
+    }
+	public void run() {
+			try {
+				numberManger.printEvenNumber();
+			}
+			catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+	}
+}
+class OddPrinterThread extends Thread{
+	NumberManger numberManger;
+	public OddPrinterThread(NumberManger numberManger) {
+		this.numberManger=numberManger;
+    }
+	public void run() {
+			try {
+				numberManger.printOddNumber();
+			}
+			catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+	}
+}
